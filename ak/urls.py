6e98c6506d20ce419afaec439b1c444e46509f96
TMexe404 <muhammadrivan888@gmail.com>
2022-05-12 04:03:34 +0700

    link image

--- ak/urls.py
+++ ak/urls.py
@@ -5,4 +5,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apie.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
